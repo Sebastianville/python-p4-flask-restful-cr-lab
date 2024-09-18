1dd82c4c651a942aa829d5420c0cb58e600701b1
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-09-18 18:10:00 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -17,10 +17,33 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        que = Plant.query.all()
+        if(not que):
+            return make_response({'message': 'plant not found'}, 404)
+        plant_dict = [r.to_dict() for r in que]
+        return make_response(plant_dict, 200)
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            plant = Plant(name=data.get('name'), image=data.get('image'), price=data.get('price'))
+            db.session.add(plant)
+            db.session.commit()
+            return make_response(plant.to_dict(), 201)
+        except: 
+            return make_response({'message': 'Post did not go through, plant was not added'}, 422)
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+     def get(self, id):
+        que = Plant.query.filter_by(id=id).first()
+        if(not que):
+            return make_response({'message': f'plant {id} not found'}, 404)
+        return make_response(que.to_dict(), 200)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
